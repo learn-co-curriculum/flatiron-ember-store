e3dc5f1fdb625182c4782bde1fbb6368f3ddb6c0
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-01-20 15:47:04 -0500

    added integration tests and passing them test that you can visit /products, click product name and see that product's details

---
+++ tests/integration/user-interaction-test.js
@@ -0,0 +1,33 @@
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+describe('Integration: User Interaction', function() {
+  var application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    Ember.run(application, 'destroy');
+  });
+
+  it('shows the product details when the user clicks on the product name', function(){
+    visit('/products');
+    click('h4:first');
+    
+    andThen(function() {
+      expect(find('h3').text()).to.eq('My Awesome Product');
+      expect(find('p').text()).to.include('price: $10000');
+      expect(find('p').text()).to.include('description: something');
+    });
+  });
+
+});
